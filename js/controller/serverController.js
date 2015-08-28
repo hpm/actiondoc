6c54a13657a2907b57db097ecbb76ad8692c5ced
Mario Micklisch <mario@favo.org>
2015-08-28 21:23:30 +0200

    error message in case the server is not available

--- js/controller/serverController.js
+++ js/controller/serverController.js
@@ -1,11 +1,13 @@
 angular.module('ActionDocApp')
   .controller('ServerCtrl', function($scope, $location, $localStorage, ActionDocService) {
+    $scope.status = {};
 
     // storage of previously used urls
     $scope.lastDocs = $localStorage.lastDocs || null;
 
     // load url
     $scope.testUrl = function(serverUrl) {
+        $scope.status = {};
         ActionDocService.getDoc(serverUrl)
             .then(function(status) {
                 var url = ActionDocService.getUrl();
@@ -18,11 +20,15 @@ angular.module('ActionDocApp')
                         url: ActionDocService.getUrl()
                     });
                 }
-                
+
                 $location.path("/app");
                 $location.search('url', url);
             })
             .catch(function(error) {
+                $scope.status = {
+                    errorCode: 1,
+                    message: 'Could not load documentation from ' + serverUrl
+                };
                 console.log('Error', error);
             })
             ;
